e95d73fa764245e16f64c15f62bf742a99d5cfc8
sang <kipkuruicollinssang@gmail.com>
2022-11-07 12:15:55 +0300

    Q3 string aaa and bbb

---
+++ Q3 string aaa and bbb/src/Main.java
@@ -0,0 +1,15 @@
+
+
+/**
+ * Collins K. Sang
+ * $DATE $TIME
+ * $PROJECT_NAME
+ * $NAME
+ * $PRODUCT_NAME
+ **/
+public class Main {
+    public static void main(String[] args) {
+        StringReturns stringReturns = new StringReturns();
+        System.out.println(stringReturns.strWithout3a3b(1,4));
+    }
+}
---
+++ Q3 string aaa and bbb/src/StringReturns.java
@@ -0,0 +1,36 @@
+/**
+ * Collins K. Sang
+ * 11/7/22 12:02 PM
+ * Q3 string aaa and bbb
+ * StringReturns
+ * IntelliJ IDEA
+ **/
+public class StringReturns {
+
+        public String strWithout3a3b(int A, int B) {
+            StringBuilder ans = new StringBuilder();
+
+            while (A > 0 || B > 0) {
+                boolean writeA = false;
+                int L = ans.length();
+                if (L >= 2 && ans.charAt(L-1) == ans.charAt(L-2)) {
+                    if (ans.charAt(L-1) == 'b')
+                        writeA = true;
+                } else {
+                    if (A >= B)
+                        writeA = true;
+                }
+
+                if (writeA) {
+                    A--;
+                    ans.append('a');
+                } else {
+                    B--;
+                    ans.append('b');
+                }
+            }
+
+            return ans.toString();
+        }
+
+}
